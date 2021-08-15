4eeb4ae1c623af7af13f613571f318946bc952f7
gyg <1330439415@qq.com>
2021-08-15 12:23:34 +0800

    third commit

--- src/main/java/com/gyg/git/GitTest.java
+++ src/main/java/com/gyg/git/GitTest.java
@@ -3,6 +3,8 @@ package com.gyg.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
+
     }
 }
